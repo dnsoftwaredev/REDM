735d27d73e8ca2e3aa5e149c9cc79aa993db9b71
Danh <dnsoftwarestore@gmail.com>
2021-03-17 12:36:34 -0500

    fix some minor bug

--- seeds/index.js
+++ seeds/index.js
@@ -27,7 +27,8 @@ const seedDB = async () => {
             location: `${city_list[rng].city}, ${city_list[rng].state}`,
             title: `${random_selector(descriptors)} ${random_selector(p_types)}`,
             image: 'https://images.unsplash.com/photo-1512915922686-57c11dde9b6b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1353&q=80',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis id inventore placeat odio dignissimos qui eligendi nobis fugiat, ea tempore soluta architecto. Unde esse delectus, porro placeat temporibus cumque incidunt. Excepturi reiciendis facilis cupiditate voluptatum possimus inventore, veniam explicabo ut doloribus, quibusdam harum cumque necessitatibus quas a odio repudiandae, tempora atque voluptatibus error in quis magnam quod! Omnis, illum eos.'
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis id inventore placeat odio dignissimos qui eligendi nobis fugiat, ea tempore soluta architecto. Unde esse delectus, porro placeat temporibus cumque incidunt. Excepturi reiciendis facilis cupiditate voluptatum possimus inventore, veniam explicabo ut doloribus, quibusdam harum cumque necessitatibus quas a odio repudiandae, tempora atque voluptatibus error in quis magnam quod! Omnis, illum eos.',
+            price: price,
         })
         await property.save();
     }
